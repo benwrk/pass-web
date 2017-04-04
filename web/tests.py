9b013375144b8d65da31959ce3ad37fe04a15c5d
Benjapol Worakan <benwrk@live.com>
2017-04-04 21:31:19 +0700

    Add filter-test and SVG logo

--- web/tests.py
+++ web/tests.py
@@ -6,6 +6,8 @@ Replace this with more appropriate tests for your application.
 """
 
 import django
+import json
+from web.views import process_filters
 from django.test import TestCase
 
 # TODO: Configure your database in settings.py and sync before running tests.
@@ -23,3 +25,17 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class GroupingTest(TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        django.setup()
+    
+    def test_process_filters(self):
+        self.maxDiff = None
+        json_str = '{"type":"grouping-configuration","name":"Placeholder Grouping","floors":[{"name":"Floor 1","groups":[{"name":"Group 1","wards":[{"name":"Ward 1","boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}]},{"name":"Ward 2","boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}]}]},{"name":"Group 2","wards":[{"name":"Ward 1","boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}]},{"name":"Ward 2","boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}]}]}]},{"name":"Floor 2","groups":[{"name":"Group 1","wards":[{"name":"Ward 1","boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}]},{"name":"Ward 2","boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}]}]},{"name":"Group 2","wards":[{"name":"Ward 1","boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}]},{"name":"Ward 2","boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}]}]}]}]}'
+        json_obj = json.loads(json_str)
+        process_filters(json_obj)
+        self.assertEqual(json.loads(json.dumps(json_obj)), json.loads('{"floors":[{"selected":true,"name":"Floor 1","groups":[{"selected":true,"wards":[{"boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}],"selected":true,"name":"Ward 1"},{"boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}],"selected":true,"name":"Ward 2"}],"name":"Group 1"},{"selected":true,"wards":[{"boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}],"selected":true,"name":"Ward 1"},{"boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}],"selected":true,"name":"Ward 2"}],"name":"Group 2"}]},{"selected":true,"name":"Floor 2","groups":[{"selected":true,"wards":[{"boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}],"selected":true,"name":"Ward 1"},{"boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}],"selected":true,"name":"Ward 2"}],"name":"Group 1"},{"selected":true,"wards":[{"boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}],"selected":true,"name":"Ward 1"},{"boxes":[{"name":"Box 1","address":"1A2B3C4D5E6F"},{"name":"Box 2","address":"1A2B3C4D5E6F"},{"name":"Box 3","address":"1A2B3C4D5E6F"}],"selected":true,"name":"Ward 2"}],"name":"Group 2"}]}],"type":"grouping-configuration","name":"Placeholder Grouping"}'))
+    
