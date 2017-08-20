15e9c2d1e14c7b885fb0687a62068427d3d1c486
Benjapol Worakan <benwrk@live.com>
2017-08-21 02:45:48 +0300

    Reconfigure URL configs for class-based views

--- service_app/urls.py
+++ service_app/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from service_app import views
 
 urlpatterns = [
-    url(r'^floors/$', views.floor_list),
-    url(r'^floors/(?P<pk>[0-9]+)/$', views.floor_detail),
+    url(r'^floors/$', views.FloorList.as_view()),
+    url(r'^floors/(?P<pk>[0-9]+)/$', views.FloorDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
