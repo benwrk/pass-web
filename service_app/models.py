6b882c7a0b0cbcd709e3800a7b9e62795a146023
Benjapol Worakan <benwrk@live.com>
2017-08-21 02:44:45 +0300

    Add reverse relationships to models

--- service_app/models.py
+++ service_app/models.py
@@ -8,14 +8,14 @@ class Floor(models.Model):
 
 class Group(models.Model):
     name = models.CharField(max_length=50)
-    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE, related_name='groups')
 
     def __str__(self):
         return 'Group: ' + self.name
 
 class Ward(models.Model):
     name = models.CharField(max_length=50)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='wards')
 
     def __str__(self):
         return 'Ward: ' + self.name
@@ -23,7 +23,7 @@ class Ward(models.Model):
 class Box(models.Model):
     name = models.CharField(max_length=50)
     mac_address = models.CharField(max_length=12, unique=True)
-    ward = models.ForeignKey(Ward, on_delete=models.CASCADE)
+    ward = models.ForeignKey(Ward, on_delete=models.CASCADE, related_name='boxes')
 
     def __str__(self):
         return 'Box: ' + self.name + ' @ ' + self.mac_address
