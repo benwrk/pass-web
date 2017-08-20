95c9c17f7c573e8a4173350fa47012958c2157ad
Benjapol Worakan <benwrk@live.com>
2017-08-21 02:48:10 +0300

    Update __str__() for models

--- service_app/models.py
+++ service_app/models.py
@@ -4,21 +4,21 @@ class Floor(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return 'Floor: ' + self.name
+        return self.name
 
 class Group(models.Model):
     name = models.CharField(max_length=50)
     floor = models.ForeignKey(Floor, on_delete=models.CASCADE, related_name='groups')
 
     def __str__(self):
-        return 'Group: ' + self.name
+        return self.name
 
 class Ward(models.Model):
     name = models.CharField(max_length=50)
     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='wards')
 
     def __str__(self):
-        return 'Ward: ' + self.name
+        return  self.name
 
 class Box(models.Model):
     name = models.CharField(max_length=50)
@@ -26,4 +26,4 @@ class Box(models.Model):
     ward = models.ForeignKey(Ward, on_delete=models.CASCADE, related_name='boxes')
 
     def __str__(self):
-        return 'Box: ' + self.name + ' @ ' + self.mac_address
+        return self.name + ' @ ' + self.mac_address
