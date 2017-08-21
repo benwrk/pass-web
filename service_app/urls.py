2af0af834716220f1607524634432c25ec2e028d
Benjapol Worakan <benwrk@live.com>
2017-08-21 05:54:10 +0300

    Convert to use explicit URL conf and routers

--- service_app/urls.py
+++ service_app/urls.py
@@ -1,18 +1,14 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from service_app import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'floors', views.FloorViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'wards', views.WardViewSet)
+router.register(r'boxes', views.BoxViewSet)
 
 urlpatterns = [
-    url(r'^floors/$', views.FloorList.as_view(), name='floor-list'),
-    url(r'^floors/(?P<pk>[0-9]+)/$', views.FloorDetail.as_view(), name='floor-detail'),
-    url(r'^groups/$', views.GroupList.as_view(), name='group-list'),
-    url(r'^groups/(?P<pk>[0-9]+)/$', views.GroupDetail.as_view(), name='group-detail'),
-    url(r'^wards/$', views.WardList.as_view(), name='ward-list'),
-    url(r'^wards/(?P<pk>[0-9]+)/$', views.WardDetail.as_view(), name='ward-detail'),
-    url(r'^boxes/$', views.BoxList.as_view(), name='box-list'),
-    url(r'^boxes/(?P<pk>[0-9]+)/$', views.BoxDetail.as_view(), name='box-detail'),
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', views.api_root),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
